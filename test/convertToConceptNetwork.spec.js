5032455a25abd4409d2698b27226bb8ea6ae2072
François Parmentier <francois.parmentier@gmail.com>
2018-12-08 20:34:20 +0100

    :white_check_mark: Add a test

--- test/convertToConceptNetwork.spec.js
+++ test/convertToConceptNetwork.spec.js
@@ -103,5 +103,55 @@ describe('convertToConceptNetwork', () => {
             done();
         })
         .on('error', done);
+    });
+
+    it('should return a linked concept network with right coOcc and occ', (done) => {
+        from([
+            { title: 'Eliza', author: 'Weizenbaum' },
+            { title: 'Computing machinery and intelligence', author: 'Turing' },
+            { title: 'Computer power and human reason: From judgment to calculation', author: 'Weizenbaum' }
+        ])
+        .pipe(ezs('convertToConceptNetwork'))
+        .on('data', (cn) => {
+            expect(cn.node[1]).toEqual({ id: 1, label: 'title:Eliza', occ: 1 });
+            expect(cn.node[2]).toEqual({ id: 2, label: 'author:Weizenbaum', occ: 2 });
+            expect(cn.node[3]).toEqual({ id: 3, label: 'title:Computing machinery and intelligence', occ: 1 });
+            expect(cn.node[4]).toEqual({ id: 4, label: 'author:Turing', occ: 1 });
+            expect(cn.node[5]).toEqual({ id: 5, label: 'title:Computer power and human reason: From judgment to calculation', occ: 1 });
+            expect(cn.link).toEqual({
+                '1_2': { coOcc: 1, fromId: 1, toId: 2 },
+                '2_1': { coOcc: 1, fromId: 2, toId: 1 },
+                '2_5': { coOcc: 1, fromId: 2, toId: 5 },
+                '3_4': { coOcc: 1, fromId: 3, toId: 4 },
+                '4_3': { coOcc: 1, fromId: 4, toId: 3 },
+                '5_2': { coOcc: 1, fromId: 5, toId: 2 }
+            });
+            done();
+        })
+        .on('error', done);
+    })
+
+    it('should return a linked concept network with right coOcc', (done) => {
+        from([
+            { title: 'Eliza', author: 'Weizenbaum', year: 1966 },
+            { title: 'Computing machinery and intelligence', author: 'Turing', year: 2009 },
+            { title: 'Computer power and human reason: From judgment to calculation', author: 'Weizenbaum', year: 1976 },
+            { title: 'Is verbal–spatial binding in working memory impaired by a concurrent memory load?', author: 'Parmentier', year: 2009 }
+        ])
+        .pipe(ezs('convertToConceptNetwork'))
+        .on('data', (cn) => {
+            expect(cn.node[1]).toEqual({ id: 1, label: 'title:Eliza', occ: 1 });
+            expect(cn.node[2]).toEqual({ id: 2, label: 'author:Weizenbaum', occ: 2 });
+            expect(cn.node[3]).toEqual({ id: 3, label: 'year:1966', occ: 1 });
+            expect(cn.node[4]).toEqual({ id: 4, label: 'title:Computing machinery and intelligence', occ: 1 });
+            expect(cn.node[5]).toEqual({ id: 5, label: 'author:Turing', occ: 1 });
+            expect(cn.node[6]).toEqual({ id: 6, label: 'year:2009', occ: 2 });
+            expect(cn.link['6_5']).toEqual({ coOcc: 1, fromId: 6, toId: 5 });
+            expect(cn.link['6_10']).toEqual({ coOcc: 1, fromId: 6, toId: 10 });
+            expect(cn.link['5_6']).toEqual({ coOcc: 1, fromId: 5, toId: 6 });
+            expect(cn.link['10_6']).toEqual({ coOcc: 1, fromId: 10, toId: 6 });
+            done();
+        })
+        .on('error', done);
     })
 });
