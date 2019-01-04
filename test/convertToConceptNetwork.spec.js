c876df1ce8079e87c73369264a0120dfc3be141c
François Parmentier <francois.parmentier@gmail.com>
2019-01-04 17:31:28 +0100

    :rotating_light: Adapt tests

--- test/convertToConceptNetwork.spec.js
+++ test/convertToConceptNetwork.spec.js
@@ -1,7 +1,7 @@
 import from from 'from';
 import ezs from 'ezs';
 import statements from '../lib';
-import { ConceptNetwork } from 'concept-network';
+import { cnGetLink } from 'concept-network/lib/concept-network';
 
 ezs.use(statements);
 
@@ -72,7 +72,9 @@ describe('convertToConceptNetwork', () => {
         from([{ id: 1 }])
         .pipe(ezs('convertToConceptNetwork'))
         .on('data', (cn) => {
-            expect(cn).toBeInstanceOf(ConceptNetwork);
+            expect(cn).toEqual({
+                node: [{ label: 'id:1', occ: 1}]
+            });
             done();
         })
         .on('error', done);
@@ -82,18 +84,10 @@ describe('convertToConceptNetwork', () => {
         from([{ id: 1 }])
         .pipe(ezs('convertToConceptNetwork'))
         .on('data', (cn) => {
-            expect(cn).toBeInstanceOf(ConceptNetwork);
+            expect(cn).toHaveProperty('node');
             expect(cn.node).not.toEqual({});
-            expect(cn.node[1]).toEqual({ id: 1, label: 'id:1', occ: 1});
             expect(cn).toEqual({
-                fromIndex: {},
-                labelIndex: { 'id:1': 1 },
-                link: {},
-                node: {
-                    1: { id: 1, label: 'id:1', occ: 1 }
-                }, 
-                nodeLastId: 1,
-                toIndex: {}
+                node: [{ label: 'id:1', occ: 1 }]
             })
             done();
         })
@@ -104,12 +98,12 @@ describe('convertToConceptNetwork', () => {
         from([{ title: 'Eliza', author: 'Weizenbaum' }])
         .pipe(ezs('convertToConceptNetwork'))
         .on('data', (cn) => {
-            expect(cn.node[1]).toEqual({ id: 1, label: 'title:Eliza', occ: 1});
-            expect(cn.node[2]).toEqual({ id: 2, label: 'author:Weizenbaum', occ: 1});
-            expect(cn.link).toEqual({
-                '1_2': { coOcc: 1, fromId: 1, toId: 2 },
-                '2_1': { coOcc: 1, fromId: 2, toId: 1 }
-            });
+            expect(cn.node[0]).toEqual({ label: 'title:Eliza', occ: 1});
+            expect(cn.node[1]).toEqual({ label: 'author:Weizenbaum', occ: 1});
+            expect(cn.link).toEqual([
+                { coOcc: 1, from: 0, to: 1 },
+                { coOcc: 1, from: 1, to: 0 }
+            ]);
             done();
         })
         .on('error', done);
@@ -123,19 +117,21 @@ describe('convertToConceptNetwork', () => {
         ])
         .pipe(ezs('convertToConceptNetwork'))
         .on('data', (cn) => {
-            expect(cn.node[1]).toEqual({ id: 1, label: 'title:Eliza', occ: 1 });
-            expect(cn.node[2]).toEqual({ id: 2, label: 'author:Weizenbaum', occ: 2 });
-            expect(cn.node[3]).toEqual({ id: 3, label: 'title:Computing machinery and intelligence', occ: 1 });
-            expect(cn.node[4]).toEqual({ id: 4, label: 'author:Turing', occ: 1 });
-            expect(cn.node[5]).toEqual({ id: 5, label: 'title:Computer power and human reason: From judgment to calculation', occ: 1 });
-            expect(cn.link).toEqual({
-                '1_2': { coOcc: 1, fromId: 1, toId: 2 },
-                '2_1': { coOcc: 1, fromId: 2, toId: 1 },
-                '2_5': { coOcc: 1, fromId: 2, toId: 5 },
-                '3_4': { coOcc: 1, fromId: 3, toId: 4 },
-                '4_3': { coOcc: 1, fromId: 4, toId: 3 },
-                '5_2': { coOcc: 1, fromId: 5, toId: 2 }
-            });
+            expect(cn.node[0]).toEqual({ label: 'title:Eliza', occ: 1 });
+            expect(cn.node[1]).toEqual({ label: 'author:Weizenbaum', occ: 2 });
+            expect(cn.node[2]).toEqual({ label: 'title:Computing machinery and intelligence', occ: 1 });
+            expect(cn.node[3]).toEqual({ label: 'author:Turing', occ: 1 });
+            expect(cn.node[4]).toEqual({ label: 'title:Computer power and human reason: From judgment to calculation', occ: 1 });
+            expect(cnGetLink(cn, 'title:Eliza', 'author:Weizenbaum')).toEqual({ coOcc: 1, from: 0, to: 1 });
+            expect(cnGetLink(cn, 'author:Weizenbaum', 'title:Eliza')).toEqual({ coOcc: 1, from: 1, to: O });
+            expect(cnGetLink(cn, 'author:Weizenbaum', 'title:Computer power and human reason: From judgment to calculation'))
+                .toEqual({ coOcc: 1, from: 1, to: 4 });
+            expect(cnGetLink(cn, 'title:Computing machinery and intelligence', 'author:Turing'))
+                .toEqual({ coOcc: 1, from: 2, to: 3 });
+            expect(cnGetLink(cn, 'author:Turing', 'title:Computing machinery and intelligence'))
+                .toEqual({ coOcc: 1, from: 3, to: 2 });
+            expect(cnGetLink(cn, 'title:Computer power and human reason: From judgment to calculation', 'author:Weizenbaum'))
+                .toEqual({ coOcc: 1, from: 4, to: 1 });
             done();
         })
         .on('error', done);
@@ -149,17 +145,18 @@ describe('convertToConceptNetwork', () => {
             { title: 'Is verbal–spatial binding in working memory impaired by a concurrent memory load?', author: 'Parmentier', year: 2009 }
         ])
         .pipe(ezs('convertToConceptNetwork'))
+        .pipe(ezs('debug'))
         .on('data', (cn) => {
-            expect(cn.node[1]).toEqual({ id: 1, label: 'title:Eliza', occ: 1 });
-            expect(cn.node[2]).toEqual({ id: 2, label: 'author:Weizenbaum', occ: 2 });
-            expect(cn.node[3]).toEqual({ id: 3, label: 'year:1966', occ: 1 });
-            expect(cn.node[4]).toEqual({ id: 4, label: 'title:Computing machinery and intelligence', occ: 1 });
-            expect(cn.node[5]).toEqual({ id: 5, label: 'author:Turing', occ: 1 });
-            expect(cn.node[6]).toEqual({ id: 6, label: 'year:2009', occ: 2 });
-            expect(cn.link['6_5']).toEqual({ coOcc: 1, fromId: 6, toId: 5 });
-            expect(cn.link['6_10']).toEqual({ coOcc: 1, fromId: 6, toId: 10 });
-            expect(cn.link['5_6']).toEqual({ coOcc: 1, fromId: 5, toId: 6 });
-            expect(cn.link['10_6']).toEqual({ coOcc: 1, fromId: 10, toId: 6 });
+            expect(cn.node[0]).toEqual({ label: 'title:Eliza', occ: 1 });
+            expect(cn.node[1]).toEqual({ label: 'author:Weizenbaum', occ: 2 });
+            expect(cn.node[2]).toEqual({ label: 'year:1966', occ: 1 });
+            expect(cn.node[3]).toEqual({ label: 'title:Computing machinery and intelligence', occ: 1 });
+            expect(cn.node[4]).toEqual({ label: 'author:Turing', occ: 1 });
+            expect(cn.node[5]).toEqual({ label: 'year:2009', occ: 2 });
+            expect(cnGetLink(cn, 'year:2009', 'author:Turing')).toEqual({ coOcc: 1, from: 5, to: 4 });
+            expect(cnGetLink(cn, 'year:2009', 'author:Parmentier')).toEqual({ coOcc: 1, from: 5, to: 9 });
+            expect(cnGetLink(cn, 'author;Turing', 'year:2009')).toEqual({ coOcc: 1, from: 4, to: 5 });
+            expect(cnGetLink(cn, 'author:Parmentier', 'year:2009')).toEqual({ coOcc: 1, from: 9, to: 5 });
             done();
         })
         .on('error', done);
